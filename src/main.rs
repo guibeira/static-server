ce6080517c80eea63cc667044a84581b949dfa5f
GuilhermeVBeira <guilherme.vieira.beira@gmail.com>
2021-09-21 11:58:55 -0300

    Rust fmt

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,17 @@
-use std::{fs, thread};
+use clap::{App, Arg};
 use env_logger::Env;
 use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
-use clap::{App, Arg};
+use std::{fs, thread};
 
 #[macro_use]
 extern crate log;
 
-mod contants;
-use contants::{HTTP_NOT_ALLOWED, HTTP_NOT_FOUND, HTTP_OK, PROTOCOL};
+mod constants;
+use constants::{HTTP_NOT_ALLOWED, HTTP_NOT_FOUND, HTTP_OK, PROTOCOL};
 
-
-fn main(){
+fn main() {
     env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
 
     let matches = App::new("static-server")
@@ -20,50 +19,46 @@ fn main(){
         .arg(
             Arg::new("PORT")
                 .about("port where the server  will run")
-                .default_value("8080")
+                .default_value("8080"),
         )
         .arg(
             Arg::new("FOLDER")
                 .about("The path o the folder to be served")
-                .default_value(".")
+                .default_value("."),
         )
         .get_matches();
 
     let port = matches.value_of("PORT").unwrap();
-    let folder =  String::from(matches.value_of("FOLDER").unwrap());
+    let folder = String::from(matches.value_of("FOLDER").unwrap());
 
     info!("Starting server at http://127.0.0.1:{}/", port);
-
     let listener = TcpListener::bind(format!("localhost:{}", port)).unwrap();
-    for stream in listener.incoming(){
+    for stream in listener.incoming() {
         let html_folder = folder.clone();
         let stream = stream.unwrap();
-        thread::spawn(move || {
-            handle_connection(stream, &html_folder)
-        });
+        thread::spawn(move || handle_connection(stream, &html_folder));
     }
 }
 
-fn handle_connection(mut stream: TcpStream, html_folder: &str){
+fn handle_connection(mut stream: TcpStream, html_folder: &str) {
     let mut buffer = [0; 1024];
     stream.read(&mut buffer).unwrap();
     let request_data = String::from_utf8_lossy(&buffer[..]);
     let request_data_lines: Vec<&str> = request_data.split('\n').collect();
     let first_line = request_data_lines[0];
     let start: Vec<&str> = first_line.split(" ").collect();
-    
+
     let method = start[0];
     let path = start[1];
-    
+
     if method != "GET" {
         let response = format!("{} {}\r\n\r\n", PROTOCOL, HTTP_NOT_ALLOWED.full_msg());
         stream.write(response.as_bytes()).unwrap();
         stream.flush().unwrap();
-        info!("{} {} HTTP/1.1 {}",method, path, HTTP_NOT_ALLOWED.code);
-        return
+        info!("{} {} HTTP/1.1 {}", method, path, HTTP_NOT_ALLOWED.code);
+        return;
     }
 
-    
     let filename = match path {
         "/" => format!("{}index.html", html_folder),
         _ => {
@@ -71,22 +66,20 @@ fn handle_connection(mut stream: TcpStream, html_folder: &str){
             format!("{}/{}.html", html_folder, filename)
         }
     };
-    
 
     let contents = match fs::read_to_string(filename) {
         Ok(filename) => filename,
         Err(_e) => {
             let new_path = path.replacen("/", "", 1);
-            fs::read_to_string(format!("{}/{}/index.html",html_folder, new_path)).unwrap_or("Not Found".to_string())
-        },
+            fs::read_to_string(format!("{}/{}/index.html", html_folder, new_path))
+                .unwrap_or("Not Found".to_string())
+        }
     };
-    
 
     let status_line = match contents.as_str() {
         "Not Found" => HTTP_NOT_FOUND.full_msg(),
         _ => HTTP_OK.full_msg(),
-    }; 
-
+    };
 
     let response = format!(
         "{} {}\r\nContent-Length: {}\r\n\r\n{}",
@@ -95,9 +88,8 @@ fn handle_connection(mut stream: TcpStream, html_folder: &str){
         contents.len(),
         contents
     );
-    
-    info!("{} {} {} {}",method, path, PROTOCOL, status_line);
+
+    info!("{} {} {} {}", method, path, PROTOCOL, status_line);
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
-
 }
